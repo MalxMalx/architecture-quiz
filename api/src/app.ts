c9d30d5d67803324ec86ed987b5bdf83d666dd9b
MalxMalx <alexandrmalka@gmail.com>
2018-10-15 21:25:27 +0300

    Serve with webpack

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,7 +1,5 @@
 import Koa from 'koa';
-import path from 'path';
 import bodyParser from 'koa-body';
-import serve from 'koa-static';
 import quiz from './routes/quiz';
 import user from './routes/user';
 import question from './routes/question';
@@ -12,6 +10,5 @@ app.use(bodyParser());
 app.use(quiz.routes());
 app.use(user.routes());
 app.use(question.routes());
-app.use(serve(path.join(__dirname, '../../client/build')));
 
 export default app;
