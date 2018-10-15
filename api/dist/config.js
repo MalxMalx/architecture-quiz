bf589a3e4626bc9e7af3459d64c89fdfc323a6aa
MalxMalx <alexandrmalka@gmail.com>
2018-10-15 17:49:30 +0300

    Add dist to gitignore

--- api/dist/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    questionsPerQuiz: 10,
-    dbName: 'architecture-quiz',
-    mongoUrl: 'mongodb+srv://api:wSG58oIq3GCDGM7h@architecturequizcluster0-cwrxa.mongodb.net/test?retryWrites=true'
-};
--- api/dist/helpers/mongo.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const util_1 = require("util");
-exports.findOne = (collection, ...args) => util_1.promisify(collection.findOne).apply(collection, args);
-exports.insertOne = (collection, ...args) => util_1.promisify(collection.insertOne).apply(collection, args);
